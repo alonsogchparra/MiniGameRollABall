913fdf9760be447c5e54ebccda589cdfec6c0623
Alonsop <alonsop@uakami.com>
2018-05-06 13:04:40 -0400

    Adding OnTriggerEnter, make pickups set active false and show text when is start taking pickups and show message when player wins

--- Assets/Codes/PlayerController.cs
+++ Assets/Codes/PlayerController.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public int speed;
+	public Text countText, winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	// Use this for initialization
 	void Start () {
 
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		winText.text = "";
+		SetCountText();
 		
 	}
 	
@@ -26,4 +32,26 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce(movement * speed);
 			
 	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if(other.gameObject.CompareTag("Pick Up")) {
+			
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+
+			if(count >= 12) {
+				winText.text = "YOU WIN!!!";
+			}
+
+		}
+
+	}
+
+
+	void SetCountText() {
+		countText.text = "Count: " + count;
+	}
+
 }
